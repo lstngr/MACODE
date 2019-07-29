48a97d4c5117a27b97897647544baaca5751b553
Louis Stenger <louis.stenger@epfl.ch>
2019-07-29 11:10:19 +0200

    Initial Commit for NegTri Project
    Included basic setup file and M2HTML documentation engine.

---
+++ NegTriSetup.m
@@ -0,0 +1,20 @@
+function NegTriSetup
+% NEGTRISETUP Sets up the negative triangularity project
+%   NEGTRISETUP makes sure required scripts and paths are added to MATLAB's
+%   userpath, builds the documentation if required, and does other things.
+
+% TODO - Add other things that NEGTRISETUP might do!
+
+%% Add the negative triangularity project to the userpath
+% Request the current's script location with <matlab:doc('mfilename') mfilename>
+% and add required subfolders to the path.
+script_path     = mfilename('fullpath');	% Get full setup path
+[script_path,~] = fileparts(script_path);	% Strip the script name
+addpath( script_path,...                    % Add folders to path
+    genpath([script_path,filesep,'src']) )
+
+%% Generate M2HTML documentation for everybody!
+m2html('mfiles','src','ignoredDir','m2html',...
+    'htmldir','docs', 'recursive','on', 'global','on',...
+    'graph','on','globalHypertextLinks','on');
+end
