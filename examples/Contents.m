8c55290a364034076c34519c050ef8b1cdd5b918
Louis Stenger <louis.stenger@epfl.ch>
2019-08-10 09:39:16 +0200

    Complete previous commit with examples

---
+++ examples/Contents.m
@@ -0,0 +1,16 @@
+% EXAMPLES of the MACODE toolbox
+% MACODE Version 1.0 (R2015a) 10-Aug-2019
+%
+% Getting Started
+%   currents       - Create sample currents
+%   simpleDivertor - Working with Multiple Currents
+%   configDivertor - Create a Magnetic Configuration Object
+%   doubleXPoint   - Magnetic Configuration with Many X-Points
+%
+% Going Further
+%   configCopy     - Copying Currents and Configurations
+%   symbolicConfig - Work on Magnetic Configurations with Symbolic Variables
+%   parameterScan  - Creating Parameter Scans Over Magnetic Configurations
+%
+% You can view these examples by navigating to the toolbox's documentation
+% in the MATLAB documentation browser.
